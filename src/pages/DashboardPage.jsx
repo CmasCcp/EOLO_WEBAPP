f679ac31df791bf2f37f4da24bd48422cd1cdbeb
David Kressin <david@garciagonzales.com>
2025-06-23 15:42:23 -0400

    Consultado api en devices page

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 export const DashboardPage = () => {
   return (
-    <div>DashboardPage</div>
+    // <div>DashboardPage</div>
+    <iframe title="Sample Report Demo" width="1140" height="541.25" src="https://playground.powerbi.com/sampleReportEmbed" frameborder="0" allowFullScreen="true"></iframe>
   )
 }
--- src/pages/DevicesPage.jsx
+++ src/pages/DevicesPage.jsx
@@ -1,6 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 export const DevicesPage = () => {
+
+    useEffect(()=>{
+
+        const fetchData =async()=>{
+
+            try{
+                const response = await fetch("https://api-sensores.cmasccp.cl/listarDatos?tabla=dispositivos")
+                console.log(response);
+            }catch{
+                console.log("errr")
+            }
+            
+        }
+
+        fetchData();
+    }, [])
+
   return (
     <div>DevicesPage</div>
   )
