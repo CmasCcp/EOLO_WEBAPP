2a1f1a5de5f996929d0f62b9e2817532b9e8e99a
David Kressin <david@garciagonzales.com>
2025-06-23 15:06:49 -0400

    Desarrollo pagina login

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -2,6 +2,15 @@ import React from 'react'
 
 export const LoginPage = () => {
   return (
-    <div>LoginPage</div>
+      <div className="card">
+        <div>LoginPage</div>
+        Username
+        <input type="text" />
+        Pass
+        <input className="form-control" type="text" />
+        
+        <button className='btn btn-dark'>Login</button>
+
+    </div>
   )
 }
